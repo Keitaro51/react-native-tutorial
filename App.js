1932c79705d0c769392f5cb127d6cd4407acf128
R.Charlot <romain.charlot@lilo.org>
2023-02-07 11:57:54 +0100

    creating first layout with styling, basic components (lists, specific behaviours,...) and event handling

--- App.js
+++ App.js
@@ -1,20 +1,103 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {useState} from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+  FlatList,
+} from 'react-native'
 
 export default function App() {
+  const [enterGoalText, setEnterGoalText] = useState('')
+  const [courseGoals, setCourseGoals] = useState([])
+
+  const goalInputHandler = (enteredText) => {
+    setEnterGoalText(enteredText)
+  }
+
+  const addGoalHandler = () => {
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      {text: enterGoalText, id: Math.random().toString()},
+    ])
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your course goal!"
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
+      </View>
+      <View style={styles.goalsContainer}>
+        {/*scrollview component not recommanded for dynamic/long lists. Prefer flatlist coponent for lazy loading behaviour*/}
+        {/* <ScrollView> 
+          {courseGoals.map((goal, index) => (
+            <View
+              key={index}
+              style={
+                styles.goalItem
+              }  
+            >
+              <Text style={styles.goalText}>{goal}</Text>
+            </View>
+          ))}
+        </ScrollView> */}
+        {/*keyExtractor needed only if data have no prop called key*/}
+        <FlatList
+          data={courseGoals}
+          keyExtractor={(item, index) => item.id}
+          renderItem={(itemData) => {
+            return (
+              <View style={styles.goalItem}>
+                {/* this view wrapper component is used for ios devices where text component doesn't compile such styling props like border radius */}
+                <Text style={styles.goalText}>{itemData.item.text}</Text>
+              </View>
+            )
+          }}
+        />
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 60,
+    paddingHorizontal: 16,
   },
-});
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#CCC',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#CCC',
+    width: '70%',
+    marginRight: 8,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: '#5e0acc',
+  },
+  goalText: {
+    color: 'white',
+  },
+})
