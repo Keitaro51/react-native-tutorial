f526f1eada0136f36f3647016c99296a6d018c56
R.Charlot <romain.charlot@lilo.org>
2023-02-08 15:24:45 +0100

    some styling effect on item press

--- components/GoalItem.jsx
+++ components/GoalItem.jsx
@@ -2,13 +2,17 @@ import {Pressable, StyleSheet, Text, View} from 'react-native'
 
 const GoalItem = ({item, onDeleteItem}) => {
   return (
-    <Pressable onPress={() => onDeleteItem(item.id)}>
-      {/* Other syntax with bind funct. <Pressable onPress={onDeleteItem.bind(this, item.id)}> */}
-      <View style={styles.goalItem}>
-        {/* this view wrapper component is used for ios devices where text component doesn't compile such styling props like border radius */}
+    <View style={styles.goalItem}>
+      {/* this view wrapper component is used for ios devices where text component doesn't compile such styling props like border radius */}
+      <Pressable
+        android_ripple={{color: '#3e0885'}}
+        style={({pressed}) => pressed && styles.pressedItem} //for ios feedback
+        onPress={() => onDeleteItem(item.id)}
+      >
+        {/* Other syntax with bind funct. <Pressable onPress={onDeleteItem.bind(this, item.id)}> */}
         <Text style={styles.goalText}>{item.text}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   )
 }
 
@@ -17,11 +21,14 @@ export default GoalItem
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: '#5e0acc',
   },
   goalText: {
+    padding: 8,
     color: 'white',
+  },
+  pressedItem: {
+    opacity: 0.5,
   },
 })
